cc444478079c0522a51e3af82ec5c11a4fdceade
Thaís Silva <thaiscaroline.cms@gmail.com>
2022-03-31 18:21:49 -0300

    http request and Pokemon component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
   <div id="app">
-    
+    <div v-for="(poke, index) in pokemons" :key="index">
+    <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+    </div>
   </div>
 </template>
 
 <script>
-
+import axios from "axios";
+import Pokemon from "./components/Pokemon";
 export default {
   name: 'App',
-  components: {
-    
-  }
+  data(){
+    return{
+      pokemons: []
+    }
+  },
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
+      console.log("Pegou a lista de pokemons");
+      this.pokemons = res.data.results;
+    })
+  },
+ components: {
+   Pokemon
+ }
 }
 </script>
 
